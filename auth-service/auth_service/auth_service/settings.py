23b9932f4762814efda1c9d04b2615f0a902a704
agusrichard <agus.richard21@gmail.com>
2021-04-30 10:29:08 +0700

    feat: setup database settings for auth service using postgresql

--- auth-service/auth_service/auth_service/settings.py
+++ auth-service/auth_service/auth_service/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u#m601(pnhhv0%wlc7+fkqhbl3^y1qqtuct7c!wz1h4yoh$a7*'
+SECRET_KEY = os.environ['AUTH_SERVICE_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ['AUTH_SERVICE_DEBUG'])
 
 ALLOWED_HOSTS = ['auth-service']
 
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'auth_service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['AUTH_SERVICE_DB_NAME'],
+        'USER': os.environ['AUTH_SERVICE_DB_USER'],
+        'PASSWORD': os.environ['AUTH_SERVICE_DB_PASSWORD'],
+        'HOST': os.environ['AUTH_SERVICE_DB_HOST'],
+        'PORT': os.environ['AUTH_SERVICE_DB_PORT'],
     }
 }
 
